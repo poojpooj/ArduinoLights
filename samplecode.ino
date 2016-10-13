63337bb994326bfc7ffba8df0377e6d4d9e8c4ba
17DavisSa <17DavisSa@gmail.com>
2016-10-13 18:46:30 -0400

    Update samplecode.ino

--- samplecode.ino
+++ samplecode.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(num8 PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   strip.begin();
@@ -9,9 +9,14 @@ void setup() {
 
 void loop() {
   strip.setPixelColor(0,0,0,255);
-  strip.show();
-  delay(100);
-  strip.setPixelColor(0,0,255,0);
+  strip.setPixelColor(1,255,0,0);
+  strip.setPixelColor(2,255,0,255);
+  strip.setPixelColor(3,0,255,0);
+  strip.setPixelColor(4,0,255,255);
+  strip.setPixelColor(5,255,255,0);
+  strip.setPixelColor(6,0,255,0);
+  strip.setPixelColor(7,0,0,255);
+
   strip.show();
   delay(100);
 }
